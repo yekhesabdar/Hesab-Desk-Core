3f7074356131d3f281fdbf31aa0dfe211edaf342
emg <emg.programer@gmail.com>
2026-09-24 19:41:01 +0330

    fix: restore missing clipboard_file import and rename terminal model

--- src/server/connection.rs
+++ src/server/connection.rs
@@ -8,6 +8,8 @@ use super::{input_service::*, *};
 use crate::clipboard::try_empty_clipboard_files;
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
 use crate::clipboard::{update_clipboard, ClipboardSide};
+#[cfg(any(target_os = "windows", target_os = "linux", target_os = "macos"))]
+use crate::clipboard_file::*;
 #[cfg(target_os = "android")]
 use crate::keyboard::client::map_key_to_control_key;
 #[cfg(any(target_os = "windows", target_os = "linux"))]
